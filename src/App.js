815669b08114c4138576e50abb9385163de941de
Sandesh Deshmukh <121145544+Sandesh2704@users.noreply.github.com>
2024-03-13 01:12:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Page from './Component/Page';
+
+function App() {
+  return (
+    <Router>
+      <div className=" w-[100%] h-[100%] px-2 mb-5 md:px-16 lg:px-60 ">
+        <Page />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
