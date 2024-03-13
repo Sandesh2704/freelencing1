ff3bfef7c84b1f39081e38cf6007a73433126f91
Sandesh Deshmukh <121145544+Sandesh2704@users.noreply.github.com>
2024-03-13 10:33:12 +0530

    I change px value in app js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Page from './Component/Page';
 function App() {
   return (
     <Router>
-      <div className=" w-[100%] h-[100%] px-2 mb-5 md:px-16 lg:px-60 ">
+      <div className=" w-[100%] h-[100%] px-2 mb-5 md:px-16 lg:px-32 ">
         <Page />
       </div>
     </Router>
